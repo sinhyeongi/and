1245cacc5f2aeb25c2bf1141854d79af66a8acf4
sinhyeongi <sinhg1221@daum.net>
2022-05-25 10:49:45 +0900

    0.0

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -12,36 +13,45 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
-    private main_frag main_frag;
-    private Frag_state frag_state;
+    private main main_frag;
+    private state frag_state;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        main_frag = new main_frag();
-        frag_state = new Frag_state();
-        FragmentTransaction fragmentTransaction =
-                fragmentManager.beginTransaction();
+        main_frag = new main();
+        frag_state = new state();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame,main_frag).commitAllowingStateLoss();
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
-
+        //bottom click event
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            FragmentTransaction transaction;
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
                 switch (item.getItemId()){
                     case R.id.main:
-                        transaction.replace(R.id.frame,main_frag).commitAllowingStateLoss();
-                        break;
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame,main_frag);
+                        transaction.commitAllowingStateLoss();
+                        return true;
                     case R.id.state:
-                        transaction.replace(R.id.frame,frag_state).commitAllowingStateLoss();
-                        break;
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame,frag_state);
+                        transaction.commitAllowingStateLoss();
+                        return true;
                     case R.id.save:
-                        break;
+                        return true;
                 }
                 return false;
             }
+            //bottom click event end
         });
     }
+    //main end
+
 }
