c301953fdbfe8be66fdcc54b1dee68f96a70156b
sinhyeongi <sinhg1221@daum.net>
2022-11-23 11:11:42 +0900

    0.0

--- app/src/main/java/com/example/myapplication/Select_new.java
+++ app/src/main/java/com/example/myapplication/Select_new.java
@@ -39,19 +39,19 @@ public class Select_new extends AppCompatActivity {
             {
                 case R.id.btn_ez:
                     First_setting();
-                    editor.putInt("difficulty",1);
+                    editor.putInt("difficulty",0);
                     editor.commit();
                     Change_view(Select_Characters.class);
                     break;
                 case R.id.btn_normal:
                     First_setting();
-                    editor.putInt("difficulty",2);
+                    editor.putInt("difficulty",1);
                     editor.commit();
                     Change_view(Select_Characters.class);
                     break;
                 case R.id.btn_hard:
                     First_setting();
-                    editor.putInt("difficulty",3);
+                    editor.putInt("difficulty",2);
                     editor.commit();
                     Change_view(Select_Characters.class);
                     break;
